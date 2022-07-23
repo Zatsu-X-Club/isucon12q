3b312aea1ae2abdc342a60caf9ca953b1194dfb1
sanposhiho <44139130+sanposhiho@users.noreply.github.com>
2022-07-23 15:54:37 +0900

    f

--- go/isuports.go
+++ go/isuports.go
@@ -106,6 +106,7 @@ var pCache = &prepareCache{
 }
 
 func cachedSelectContext(tenantDB *sqlx.DB, ctx context.Context, query string, args ...interface{}) (*sqlx.Rows, error) {
+	pCache.mx.Lock()
 	stmt, ok := pCache.store[query]
 	pCache.mx.RUnlock()
 	if !ok {
