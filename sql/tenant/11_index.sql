d27da86319dbc18c8ad51cc2ce141db5ddfbc118
masibw <masi19bw@gmail.com>
2022-07-23 13:52:27 +0900

    drop index

--- sql/tenant/11_index.sql
+++ sql/tenant/11_index.sql
@@ -1,3 +1,6 @@
+DROP INDEX tenant_created;
+DROP INDEX tenant_compe;
+DROP INDEX tenant_compe_row;
 CREATE INDEX tenant_created ON competition(`tenant_id`, `created_at`);
 CREATE INDEX tenant_compe ON player_score(`tenant_id`, `player_id`);
 create index tenant_compe_row on player_score(`tenant_id`,`competition_id`,`row_num`);
