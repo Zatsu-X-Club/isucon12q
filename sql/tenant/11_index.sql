6f0e245fcfcf053a39e6480bbfc0f8180c016301
masibw <masi19bw@gmail.com>
2022-07-23 13:09:04 +0900

    row numにindex (#17)

--- sql/tenant/11_index.sql
+++ sql/tenant/11_index.sql
@@ -1,2 +1,3 @@
 CREATE INDEX tenant_created ON competition(`tenant_id`, `created_at`);
 CREATE INDEX tenant_compe ON player_score(`tenant_id`, `player_id`);
+create index tenant_compe_row on player_score(`tenant_id`,`competition_id`,`row_num`);
